75961d01808d548cd626df20438a4671927ed04e
omid <admin@gmail.com>
2022-04-07 18:42:53 +0300

    finished newletters

--- src/app/services/eshop-contact-us.service.ts
+++ src/app/services/eshop-contact-us.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { IContactForm } from './../interfaces/contact_us.form.interface';
 import { Observable } from 'rxjs';
 import { ISocialMedia } from '../interfaces/social_medias.interface';
+import { INewletters } from './../interfaces/newletters.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +16,23 @@ export class EshopContactUsService {
   {
     const headers = {'Content-Type':'application/json'}  
     const body=JSON.stringify(contact_form);
-    console.log("------------------")
-    console.log(body)
+    // console.log("------------------")
+    // console.log(body)
     return this.http.post("/contact_us/", body,{'headers':headers})
   }
 
-  getSocialMedias(): Observable<ISocialMedia[]> {
+  getSocialMedias(): Observable<ISocialMedia[]>
+  {
     return this.http.get<ISocialMedia[]>('/eshop_social_media');
-}
+  }
+
+  createNewNewletters(newletters_form:INewletters)
+  {
+    const headers = {'Content-Type':'application/json'}  
+    const body=JSON.stringify(newletters_form);
+    return this.http.post("/newletters/", body,{'headers':headers})
+  
+  }
 
   
 }
--- src/app/shared_components/eshop-footer/eshop-footer.component.ts
+++ src/app/shared_components/eshop-footer/eshop-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EshopContactUsService } from './../../services/eshop-contact-us.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';  
 
 @Component({
   selector: 'app-eshop-footer',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EshopFooterComponent implements OnInit {
 
-  constructor() { }
+  form_group= new FormGroup({
+    email:new FormControl(null,[Validators.minLength(13),Validators.maxLength(40),Validators.email])
+  })
+  constructor(private contactUsService:EshopContactUsService) { }
 
   ngOnInit(): void {
+  }
+  handlerNewlettersForm()
+  {
+    const data= this.form_group.value
+    this.contactUsService.createNewNewletters(data).subscribe(val=>{
+      Swal.fire({  
+        position: "center",  
+        icon: 'success',  
+        title: 'ADD YOUR EMIAL TO NEWLETTERS',  
+        showConfirmButton: true,  
+        timer: 5000
+      })  
+        
+  
+       this.form_group.reset()
+    },er=>
+    {
+      if(er.error.email == "newsletters with this email already exists.")
+      {
+        Swal.fire({  
+          position: "center",  
+          icon: 'error',  
+          title: 'YOUR EMAIL IS EXIST',  
+          showConfirmButton: true,  
+          timer: 5000
+        })  
+      }
+      else{
+        Swal.fire({  
+          position: "center",  
+          icon: 'error',  
+          title: 'EXTERNAL ERROR PLEASE TRY AGAIN',  
+          showConfirmButton: true,  
+          timer: 5000
+        })  
+
+      }
+    })
+    
+    
   }
 
 }
